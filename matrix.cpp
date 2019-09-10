390c8feb4cbe8a01d111ec5935c29c4358a25239
cnoche <54603928+cnoche@users.noreply.github.com>
2019-09-10 12:09:34 -0500

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -4,7 +4,7 @@
 matrix::matrix(): matrix{nullptr}, row {0}, clm{0} {
 
 }
-matrix::matrix(int row, int clm): row {f}, clm{c} {
+matrix::matrix(int row, int clm): row {row}, clm{clm} {
     matrix = new int*[row];
     for (int i= 0 ; i < row; i++)
     {
@@ -12,68 +12,69 @@ matrix::matrix(int row, int clm): row {f}, clm{c} {
     }
 }
 
-void matrix::transpose(const matrix& mx) {
-    for (int i = 0; i < mx.row; ++i) {
-        for (int j = 0; j < mx.clm; ++j) {
-            matrix[i][j] = matrix[j][i];
+void matrix::transpose() {
+    for (int i = 0; i < this.row; ++i) {
+        for (int j = 0; j < this.clm; ++j) {
+            this.matrix[i][j] = this.matrix[j][i];
         }
     }
 }
 
-void matrix::matrixf(const matrix& mx){
-	cout<<“\n no of rows & columns& elements”;
-	cin>>s[0].mx.row;
-	cin>>s[0].mx.clm;
-	cin>>s[0].mx.val;
-	for(int i=1;i<=s[0].v;i++){
-		cout<<“\n row & column & element”;
-		cin>>s[i].mx.row;
-		cin>>s[i].mx.clm;
-		cin>>s[i].mx.val;
+void matrix::matrixf(int n){
+	this.matrix():row(0),col(0){
+		this.matrix.resize(row);
+		for (int i=0; i<this.matrix.size(); i++){
+			this.matrix[i].resize(col,0);
+		}
+	}
+	for(int k = 0; k<n ;k++){
+		int i,j;
+		cin>>i;
+		cin>>j;
+		cin>>val;
+		this.matrix[i][j]=val;
 	}
 }
 
-bool matrix::verf(const matrix& m1, const matrix& m2) {
-	if (m1.row.length === m2.row.length){
-		if(m1.clm.length === m2.clm.length){
+bool matrix::verf(const matrix& m2) {
+	if (this.row.length === m2.row.length && this.clm.length === m2.clm.length){
 			return TRUE;
-		}
 	}else{
 		return FALSE;
 	}
 }
 
 void matrix::scalar(int n) {
-    for (int i = 0; i<row; i++){
-        for (int j = 0; j<clm;  j++){
-            matrix[i][j]=matrix[i][j]*n;
+    for (int i = 0; i<this.row; i++){
+        for (int j = 0; j<this.clm;  j++){
+            this.matrix[i][j]=this.matrix[i][j]*n;
         }
     }
 }
 
 void matrix::matrixp(){
-    for(int i = 0 ;i<row;i++){
-        for(int j = 0 ;j <clm;j++){
-            cout<<matrix[i][j]<<" ";
+    for(int i = 0 ;i<this.row;i++){
+        for(int j = 0 ;j <this.clm;j++){
+            cout<<this.matrix[i][j]<<" ";
         }cout<<endl;
     }
 }
 
 
-void matrix::sum(const &matrix m1, const &matrix m2){
-	assert(verf(m1, m2));
-	for (int i=0; i<row; i++){
-            for (int j=0; j<clm; j++){
-                matrix[i][j]=m1[i][j]+m2[i][j];
+void matrix::sum(const &matrix m2){
+	assert(verf(m2));
+	for (int i=0; i<this.row; i++){
+            for (int j=0; j<this.clm; j++){
+                this.matrix[i][j]=this.matrix[i][j]+m2[i][j];
             }
 	}
 }
 
-void matrix::mult(const &matrix, const &matrix){
-	assert(verf(m1, m2));
-		for (int i=0; i<row; i++){
-	            for (int j=0; j<clm; j++){
-	                matrix[i][j]=m1[i][j]*m2[i][j];
+void matrix::mult(const &matrix m2){
+	assert(verf(m2));
+		for (int i=0; i<this.row; i++){
+	            for (int j=0; j<this.clm; j++){
+	                this.matrix[i][j]=this.matrix[i][j]*m2[i][j];
 	            }
 		}
 }
